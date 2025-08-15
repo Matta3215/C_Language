604f2c97e6bc55f66e2ebce1433228cc07bc25d0
Matta <159219188+Matta3215@users.noreply.github.com>
2025-08-15 14:30:06 -0300

    Add files via upload

---
+++ C#/Sala/Teste_Aula1208/Conta.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_Aula1208
+{
+    internal class Conta
+    {
+        public double Saldo { get; set; }
+
+        public int Numero { get; set; }
+
+        public Cliente Dono { get; set; }
+
+        public Conta(double saldo, int numero, Cliente dono)
+        {
+            Saldo = saldo;
+            Numero = numero;
+            Dono = dono;
+        }
+
+        public bool Saca(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                Saldo -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Valor ultrapassa o saldo da conta.");
+                return false;
+            }
+        }
+
+        public void Deposita(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Transfere(Conta destino, double valor)
+        {
+            if (VerificarTransacao(valor) == true)
+            {
+                destino.Saldo += valor;
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Valor ultrapassa o saldo da conta.");
+            }
+        }
+
+        public bool VerificarTransacao(double valor)
+        {
+            if (valor <= Saldo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
