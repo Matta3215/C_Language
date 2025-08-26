1ee5189e1098bb564b682f515fee304b1ca0fa51
Matta <159219188+Matta3215@users.noreply.github.com>
2025-08-25 23:26:06 -0300

    Add files via upload

---
+++ CSharp/Exercicios_OO-1/Verao_Capixaba_01/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Verao_Capixaba_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp_convertida;
+            Console.WriteLine("==MENU==\n [1] ºC -> ºF\n [2] ºF -> ºC\n [3] ºC -> K\n [4] K -> ºC");
+            int n_temp  = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a temperatura: ");
+            double temperatura = double.Parse(Console.ReadLine());
+
+            switch (n_temp)
+            {
+                case 1:
+                    temp_convertida = temperatura * 1.8 + 32;
+                    Console.WriteLine($"{temperatura} ºC equivale a {temp_convertida:F2} ºF");
+                    break;
+                case 2:
+                    temp_convertida = (temperatura - 32) / 1.8;
+                    Console.WriteLine($"{temperatura} ºF equivale a {temp_convertida:F2} ºC");
+                    break;
+                case 3:
+                    temp_convertida = temperatura + 273.15;
+                    Console.WriteLine($"{temperatura} ºC equivale a {temp_convertida:F2} K");
+                    break;
+                case 4:
+                    temp_convertida = temperatura - 273.15;
+                    Console.WriteLine($"{temperatura} K equivale a {temp_convertida:F2} ºC");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+}
---
+++ CSharp/Exercicios_OO-1/Verao_Capixaba_02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Verao_Capixaba_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float media = 0;
+            int i = 0;
+            while (i < 15)
+            {
+                Console.WriteLine($"Digite a temperatura do {i + 1}º dia: ");
+                int temperatura = int.Parse(Console.ReadLine());
+                if (temperatura > 28)
+                {
+                    media += temperatura;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Temperatura inválida");
+                }
+            }
+            Console.WriteLine($"Média da temperatura: {(media / i):F2}");
+        }
+    }
+}
