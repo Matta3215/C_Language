65c62243ba7db26c82a23d7b0e22d9cb9cecfc81
Matta <159219188+Matta3215@users.noreply.github.com>
2025-10-22 10:50:41 -0300

    Add files via upload

---
+++ C/Sala/Pilha.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    typedef struct cel{
+        int conteudo;
+        struct cel *seg;
+    } cel;
+    
+    typedef struct cel *Pilha;
+    
+    Pilha* cria_pilha(){
+    Pilha *pi = (Pilha*) malloc(sizeof(Pilha));
+    if(pi != NULL){
+        *pi=NULL;
+        }
+    return pi;
+    }
+    
+    int empilhar(Pilha* pilha, int x){
+    if(pilha==NULL) {return 0;}
+    cel* aux = (cel*) malloc(sizeof(cel));
+    if(aux==NULL){return 0;}
+    aux->conteudo = x;
+    aux->seg = *pilha;
+    *pilha = aux;
+    return 1;
+    }
+    
+    void imprime_pilha(Pilha* pilha) {
+    if (pilha == NULL) {
+        printf("Pilha nao criada.\n");
+        return;
+    }
+    cel* aux = *pilha;
+    printf("Pilha: ");
+    while(aux != NULL) {
+        printf("%d ", aux->conteudo);
+        aux = aux->seg;
+    }
+    printf("\n");
+}
+
+    
+int main()
+{
+    Pilha *pil;
+    pil = cria_pilha();
+    empilhar(pil, 15);
+    empilhar(pil, 75);
+    empilhar(pil, 30);
+    empilhar(pil, 45);
+    imprime_pilha(pil);
+    
+}
