9bc06ede2d8626197e22642a65df28ff5b259368
Matta <159219188+Matta3215@users.noreply.github.com>
2025-10-24 02:15:20 -0300

    Add files via upload

--- C/Sala/Pilha.c
+++ C/Sala/Pilha.c
@@ -26,29 +26,47 @@
     return 1;
     }
     
+    int desempilhar(Pilha* pilha){
+    if(pilha==NULL){return 0;}
+    if(*pilha==NULL){return 0;}
+    
+    cel *aux = *pilha;
+    *pilha = aux->seg;
+    
+    free(aux);
+    return 1;
+    }
+    
     void imprime_pilha(Pilha* pilha) {
     if (pilha == NULL) {
         printf("Pilha nao criada.\n");
         return;
     }
-    cel* aux = *pilha;
+    cel *aux = *pilha;
     printf("Pilha: ");
     while(aux != NULL) {
         printf("%d ", aux->conteudo);
         aux = aux->seg;
     }
     printf("\n");
-}
-
+    }
     
+    void mostrar_topo(Pilha* pilha){
+        if(pilha==NULL || *pilha==NULL){printf("NULL\n");}
+        printf("Topo: %d", (*pilha)->conteudo);
+    }
 int main()
 {
     Pilha *pil;
     pil = cria_pilha();
+    
     empilhar(pil, 15);
     empilhar(pil, 75);
     empilhar(pil, 30);
     empilhar(pil, 45);
     imprime_pilha(pil);
     
+    desempilhar(pil);
+    imprime_pilha(pil);
+    mostrar_topo(pil);
 }
