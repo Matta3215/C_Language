d67fe62dd8355f5dd79bd9cfaad994f8f0fc12d1
Matta <159219188+Matta3215@users.noreply.github.com>
2026-02-05 08:51:47 -0300

    Add files via upload

---
+++ C/Sala/Alunos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Aluno{
+    char nome[10];
+    int nota;
+} Aluno;
+    
+
+int main()
+{
+    Aluno a1 = {"Adolfo", 3}
+    Aluno a2 = {"Struct", 5}
+    Aluno a3 = {"Epstein", 0}
+    Aluno a4 = {"Pedro", 6}
+    Aluno a5 = {"Rotatório", 10}
+    
+    FILE *arq;
+    
+    
+
+    return 0;
+}
